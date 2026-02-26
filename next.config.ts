f2aa0a5fd15dde314796d6756c76c7fc98efd47d
ibra-85 <ibraguimd@gmail.com>
2026-02-26 23:33:25 +0100

    refactor: clean up next.config.ts by removing unused output and trailingSlash properties

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,27 @@
-import type { NextConfig } from "next";
+﻿import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: 'export',
-    trailingSlash: true,
     images: {
-        unoptimized: true, // Nécessaire avec output: 'export'
+        unoptimized: true,
         remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: 'www.w3.org',
-        },
-        {
-            protocol: 'https',
-            hostname: 'upload.wikimedia.org',
-        },
-        {
-            protocol: 'https',
-            hostname: 'assets.vercel.com',
-        },
-        {
-            protocol: 'https',
-            hostname: 'git-scm.com',
-        },
-    ]
-    }
+            {
+                protocol: "https",
+                hostname: "www.w3.org",
+            },
+            {
+                protocol: "https",
+                hostname: "upload.wikimedia.org",
+            },
+            {
+                protocol: "https",
+                hostname: "assets.vercel.com",
+            },
+            {
+                protocol: "https",
+                hostname: "git-scm.com",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
